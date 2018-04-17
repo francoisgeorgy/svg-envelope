e51b9b08393e129ffaea9b2d401c9a265cd1fe21
François Georgy <francois.georgy@gmail.com>
2018-04-17 16:11:40 +0200

    use double quotes for strings

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     output: {
-        library: 'svgEnvelope',
-        libraryTarget: 'umd'
+        library: "svgEnvelope",
+        libraryTarget: "umd"
     },
     module: {
         rules: [
